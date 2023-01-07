e8c95fc511f6416eca94b3eeacd73dbf90bfecca
sadhasivam20 <sadha2040@gmail.com>
2023-01-07 21:08:58 +0530

    global data from vercel

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 //  export const API="https://638e229daefc455fb2b60cfe.mockapi.io"
- export const API="http://localhost:4000";
-//   export const API="https//node-api-ebon-eta.vercel.app"
+//  export const API="http://localhost:4000";
+  export const API="https://node-project-y1q8.vercel.app";
